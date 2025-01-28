4d35f127a92a1388eb6f91c8ddf8fa4fd6bee96b
malathisoundarapandian90 <malathi.s@kermittech.no>
2025-01-28 15:41:32 +0100

    Committing 2 items from workspace c6ff84ef-9490-4ae1-b7c6-be106bd4cb8c

--- Fabric_usecases/Notebook 1.Notebook/notebook-content.py
+++ Fabric_usecases/Notebook 1.Notebook/notebook-content.py
@@ -11,29 +11,7 @@
 
 # CELL ********************
 
-from delta.tables import DeltaTable
-from pyspark.sql.functions import col, current_timestamp, lit, when
-
-org_temp_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campaign_Bronze_Layer.Lakehouse/Tables/salesforce/Tmp_Organisation"
-
-# Read the temporary table
-organization_tmp_df = spark.read.format("delta").load(org_temp_path)
-
-# Add the Batch_id and Load_Type columns to the temp DataFrame
-organization_temp = organization_tmp_df \
-    .withColumn("Batch_id", lit(None).cast("int")) \
-    .withColumn("Load_Type", lit(None).cast("string"))  # Default to None (null)
-
-# Overwrite the Tmp_Organisation table with the updated schema
-organization_temp.write.format("delta") \
-    .option("mergeSchema", "true") \
-    .mode("overwrite") \
-    .save(org_temp_path)
-
-
-organization_temp.show()
-
-print(f"Tmp_Organisation table updated with additional columns: Batch_id and Load_Type.")
+get_batch_id = 1
 
 # METADATA ********************
 
@@ -44,7 +22,112 @@ print(f"Tmp_Organisation table updated with additional columns: Batch_id and Loa
 
 # PARAMETERS CELL ********************
 
-get_batch_id = 0
+from delta.tables import DeltaTable
+from datetime import datetime
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from pyspark.sql.functions import col, current_timestamp, lit, when
+
+org_temp_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campaign_Bronze_Layer.Lakehouse/Tables/salesforce/Tmp_Organisation"
+tmp_control_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campaign_Bronze_Layer.Lakehouse/Tables/salesforce/Control_Table"
+
+
+batch_id = get_batch_id
+
+# Read the temporary table
+organization_tmp_df = spark.read.format("delta").load(org_temp_path)
+
+# Add the Batch_id and Load_Type columns to the temp DataFrame
+organization_temp = organization_tmp_df \
+    .withColumn("Batch_id", lit(batch_id)) \
+    .withColumn("Load_Type", lit(None).cast("string"))  # Default to None (null)
+
+# Overwrite the Tmp_Organisation table with the updated schema
+organization_temp.write.format("delta") \
+    .option("mergeSchema", "true") \
+    .mode("overwrite") \
+    .save(org_temp_path)
+
+
+# Calculate the max CreateDate
+tmp_max_createdate = organization_temp.agg({"CreatedDate": "max"}).collect()[0][0]
+tmp_max_batchid = organization_temp.agg({"Batch_id": "max"}).collect()[0][0]
+
+max_value_timestamp = datetime.strptime('1990-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+
+print(max_value_timestamp)
+
+    # Define schema for the control entry
+control_schema = StructType([
+        StructField("TableName", StringType(), True),
+        StructField("MaxValue", TimestampType(), True),
+        StructField("Batch_id", StringType(), True),
+        StructField("LastUpdated", TimestampType(), True),
+        StructField("LastUpdatedBy", StringType(), True),
+])
+
+
+
+# If there is a max value (i.e., some data exists in the temporary table)
+if tmp_max_createdate:
+    # Create the control entry with the max created date
+    tmp_control_entry = [{
+            "TableName": "Tmp_Organisation",
+            "MaxValue": tmp_max_createdate,
+            "Batch_id": tmp_max_batchid,  # Ensure batch_id is passed earlier
+            "LastUpdated": None,  # Will be replaced by current_timestamp
+            "LastUpdatedBy": ""  # Adjust based on your logic
+    }]
+    
+else:
+    # If there is no max value (empty table), set MaxValue as null
+    tmp_control_entry = [{
+            "TableName": "Tmp_Organisation",
+            "MaxValue": max_value_timestamp,
+            "Batch_id": 0,  # Ensure batch_id is passed earlier
+            "LastUpdated": None,  # Will be replaced by current_timestamp
+            "LastUpdatedBy": ""  # Adjust based on your logic
+    }]
+
+
+
+# Convert the new control entry into a DataFrame
+tmp_control_df = spark.createDataFrame(tmp_control_entry, schema=control_schema) \
+        .withColumn("LastUpdated", current_timestamp())
+
+# Read the existing control table
+tmp_control_table = DeltaTable.forPath(spark, tmp_control_path)
+
+# Merge into the control table: If entry exists, update; otherwise insert a new entry
+tmp_control_table.alias("tmp_target").merge(
+    tmp_control_df.alias("tmp_source"),
+    "tmp_target.TableName = tmp_source.TableName"
+).whenMatchedUpdate(
+    set={
+        "tmp_target.MaxValue": col("tmp_source.MaxValue"),
+        "tmp_target.BatchValue": col("tmp_source.Batch_id"),
+        "tmp_target.LastUpdated": col("tmp_source.LastUpdated"),
+        "tmp_target.LastUpdatedBy": col("tmp_source.LastUpdatedBy")
+    }
+).whenNotMatchedInsert(
+    values={
+        "tmp_target.TableName": col("tmp_source.TableName"),
+        "tmp_target.MaxValue": col("tmp_source.MaxValue"),
+        "tmp_target.BatchValue": col("tmp_source.Batch_id"),
+        "tmp_target.LastUpdated": col("tmp_source.LastUpdated"),
+        "tmp_target.LastUpdatedBy": col("tmp_source.LastUpdatedBy")
+    }
+).execute()
+
+# Print confirmation
+if tmp_max_createdate:
+    print(f"Control table updated with MaxValue: {tmp_max_createdate} for Tmp_Organization.")
+else:
+    print("Control table updated with null MaxValue for Tmp_Organization as no data was found.")
+
+
+#organization_temp.show()
+
+print(f"Tmp_Organisation table updated with additional columns: Batch_id and Load_Type.")
 
 # METADATA ********************
 
@@ -64,7 +147,7 @@ organization_temp_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campa
 organization_table_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campaign_Bronze_Layer.Lakehouse/Tables/salesforce/Organisation"
 control_table_path = "abfss://Malathi@onelake.dfs.fabric.microsoft.com/Campaign_Bronze_Layer.Lakehouse/Tables/salesforce/Control_Table"
 
-batch_id = 1 #get_batch_id
+batch_id = get_batch_id
 
 # Read the temporary table
 organization_temp_df = spark.read.format("delta").load(organization_temp_path)
